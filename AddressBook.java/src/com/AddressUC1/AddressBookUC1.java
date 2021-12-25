d588aceff293ff8970fe1bfaa2b37da5595f0d6c
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-25 18:57:49 +0530

    ADDED

---
+++ AddressBook.java/src/com/AddressUC1/AddressBookUC1.java
@@ -0,0 +1,25 @@
+package com.AddressUC1;
+
+public class AddressBookUC1 {
+	private static void AddressBook() {
+		String  firstName = "Divya";
+		String lastName = "Chourasiya";
+		String address = "Indore" ;
+		String stateName = "Madhya Pradesh";
+		String zipCode = " 455118 ";
+		String contactNum = " 8209889898 ";
+		String emailID = "divyachourasiya999@gmail.com";
+
+		System.out.println("Adding following details on address book like" + " " + "firstName :" +firstName+ " lastName :" +lastName+ " address :" +address+ " stateName :" +stateName+ " zipCode :" +zipCode+ " contactNum :" +contactNum+ " emailID :" +emailID );
+	}
+
+	public static void main (String[] args) {
+		AddressBook();
+
+	}
+
+	public void getDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ AddressBook.java/src/com/AddressUC2/ContactPerson.java
@@ -0,0 +1,10 @@
+package com.AddressUC2;
+
+import com.AddressUC1.AddressBookUC1;
+
+public class ContactPerson {
+	public static void main(String[] args) {
+		AddressBookUC1 ab = new AddressBookUC1();
+		ab.getDetails();
+	}
+}
